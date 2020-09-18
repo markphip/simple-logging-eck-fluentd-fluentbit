bfa19e6e6ca003b3d0028615c41a7989414893bd
Mark Phippard <markphip@gmail.com>
2020-09-18 12:16:35 -0400

    Change script to use the local yaml files

--- deploy.sh
+++ deploy.sh
@@ -18,69 +18,20 @@ else
 fi
 # --------------------------------------------------------------------
 
-diskSpace=2
-read -p  "How many gigabyte diskspace do you want per node?(default:2) promt with [ENTER]:" inputDiskSpace
-diskSpace="${inputDiskSpace:-$diskSpace}"
-echo -e "[${LB}Info${NC}] elastic pvc requires ${diskSpace}Gi diskspace"
-# --------------------------------------------------------------------
-
 # -------------------------- deployment start ------------------------
 
 echo -e "[${LB}Info${NC}] Install custom resource definitions and the operator with its RBAC rules"
 
-kubectl apply -f https://download.elastic.co/downloads/eck/1.2.1/all-in-one.yaml
+# kubectl apply -f https://download.elastic.co/downloads/eck/1.2.1/all-in-one.yaml
+kubectl apply -f all-in-one.yaml
 
 kubectl config set-context --current --namespace=elastic-system
 
 echo -e "[${LB}Info${NC}] deploy elasticsearch"
-
-cat <<EOF | kubectl apply -f -
-apiVersion: elasticsearch.k8s.elastic.co/v1
-kind: Elasticsearch
-metadata:
-  name: quickstart
-spec:
-  version: 7.9.0
-  http:
-    tls:
-      selfSignedCertificate:
-        disabled: true 
-  nodeSets:
-  - name: default
-    count: 1
-    config:
-      node.master: true
-      node.data: true
-      node.ingest: true
-      node.store.allow_mmap: false
-    volumeClaimTemplates:
-      - metadata:
-          name: elasticsearch-data
-        spec:
-          accessModes:
-          - ReadWriteOnce
-          resources:
-            requests:
-              storage: ${diskSpace}Gi
-EOF
+kubectl apply -f quickstart-es.yaml
 
 echo -e "[${LB}Info${NC}] deploy kibana"
-
-cat <<EOF | kubectl apply -f -
-apiVersion: kibana.k8s.elastic.co/v1
-kind: Kibana
-metadata:
-  name: quickstart
-spec:
-  http:
-    tls:
-      selfSignedCertificate:
-        disabled: true 
-  version: 7.9.0
-  count: 1
-  elasticsearchRef:
-    name: quickstart
-EOF
+kubectl apply -f quickstart-kb.yaml
 
 echo -e "[${LB}Info${NC}] deploy fluentd"
 kubectl apply -f fluentd/fluentd-cm.yaml 
@@ -102,3 +53,5 @@ kubectl rollout status deployment/quickstart-kb
 echo -e "Starting port foward for Kibana. Access at http://localhost:5601 press Ctrl-C when done"
 kubectl port-forward service/quickstart-kb-http 5601
 # --------------------------------------------------------------------
+
+kubectl config set-context --current --namespace=default
